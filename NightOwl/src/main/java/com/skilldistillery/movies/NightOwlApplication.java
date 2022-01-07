6144cb8c1bc3d8db0bfd487884b2c7662ead081a
Jordan Talbot <jordan.r.talbot@gmail.com>
2022-01-07 15:40:41 -0700

    Create Spring Boot project

---
+++ NightOwl/src/main/java/com/skilldistillery/movies/NightOwlApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightOwlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightOwlApplication.class, args);
+	}
+
+}
